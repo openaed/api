d203b42b0c40ff2718d91c6859c660c670730dcf
jxpsert <jplatenburg@outlook.com>
2025-03-29 16:03:38 +0100

    Schedule imports

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\ImportController;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    ImportController::importDefibrillators();
+})->twiceDailyAt(7, 19, 0)->timezone('Europe/Amsterdam')->name('import-defibrillators-morning')
+    ->description('Import defibrillators from OpenStreetMap every day at 07:00 and 19:00');
