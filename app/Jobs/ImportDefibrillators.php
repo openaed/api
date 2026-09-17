884ec9f39539ae5f71eae766c976332af7d72ccd
jxpsert <jplatenburg@outlook.com>
2026-09-18 00:55:35 +0200

    Remove debug

--- app/Jobs/ImportDefibrillators.php
+++ app/Jobs/ImportDefibrillators.php
@@ -20,12 +20,6 @@ class ImportDefibrillators implements ShouldQueue
 
     public function handle(): void
     {
-        Log::info('Import timezone debug', [
-            'php_timezone' => date_default_timezone_get(),
-            'laravel_timezone' => config('app.timezone'),
-            'now' => now()->toDateTimeString(),
-            'now_timezone' => now()->timezoneName,
-        ]);
         ImportController::importDefibrillators(
             $this->fullImport,
             $this->overrideRegion,
