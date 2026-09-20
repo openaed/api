14ec5bf48205f8f67814e3d0339636a585f6253a
jxpsert <jplatenburg@outlook.com>
2026-09-20 03:16:14 +0200

    Revert change to be in line with other code

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -161,8 +161,8 @@ class ImportController extends Controller
             $tags['image'] = "https://api.panoramax.xyz/api/pictures/{$tags['panoramax']}/hd.jpg";
         }
 
-        if (empty($tags['defibrillator:location'])) {
-            $tags['defibrillator:location'] = $tags['defibrillator:location:en'] ?? null;
+        if (!array_key_exists('defibrillator:location', $tags)) {
+            $tags['defibrillator:location'] = $tags['defibrillator:location:en'] ?? null; // English is most common
         }
 
         $defibrillator = Defibrillator::updateOrCreate(
